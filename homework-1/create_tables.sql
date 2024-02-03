cc534e44056e7450781db637c920ec57b55f85a3
scroco158 <scroco@mail.ru>
2024-02-03 17:55:17 +0300

    HomeWork 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY ,
+	first_name varchar(50),
+	last_name varchar(50),
+	title varchar(50),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(50),
+	contact_name varchar(50)
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id char(5) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date,
+	ship_city varchar(50)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,39 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='qwerty')
+
+try:
+    with conn:
+        with conn.cursor() as curs:
+            with open('north_data/employees_data.csv') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    tuple_to_insert = (row['employee_id'],
+                                       row['first_name'],
+                                       row['last_name'],
+                                       row['title'],
+                                       row['birth_date'],
+                                       row['notes'])
+                    curs.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', tuple_to_insert)
+
+            with open('north_data/customers_data.csv') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    tuple_to_insert = (row['customer_id'],
+                                       row['company_name'],
+                                       row['contact_name'])
+                    curs.execute('INSERT INTO customers VALUES (%s, %s, %s)', tuple_to_insert)
+
+            with open('north_data/orders_data.csv') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    tuple_to_insert = (row['order_id'],
+                                       row['customer_id'],
+                                       row['employee_id'],
+                                       row['order_date'],
+                                       row['ship_city'])
+                    curs.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', tuple_to_insert)
+finally:
+    conn.close()
